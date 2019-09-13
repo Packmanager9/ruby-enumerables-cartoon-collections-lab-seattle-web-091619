1c4e3fba5e3643d4689c27804c1bae2f52758dc6
Cole Ditzler <colejditzler@gmail.com>
2019-09-13 21:50:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -57,7 +57,7 @@ def find_the_cheese (wrds)
   k = wrds[i].split("")
   puts k.length
   i =1 +i
-  if k == "Cheddar" || k == "Gouda" || k == "Camenbert"
+  if k == "cheddar" || k == "gouda" || k == "camenbert"
    return true
  else
    return false
